508e5b89dbe9afc9cd1f235bc701d6443d49ac82
Matthew James <matthewsalgorithm@gmail.com>
2026-09-10 16:22:18 +1000

    Working MVP PoC, obviously platform dependent (and very evil)

---
+++ src/safealloca.h
@@ -0,0 +1,49 @@
+#ifndef SAFE_ALLOCA_H
+#define SAFE_ALLOCA_H
+
+#include <stddef.h>
+#include <alloca.h>
+#include <stdint.h>
+#include <sys/resource.h>
+
+#define SAFE_ALLOCA_SAFETY_MARGIN 4096
+
+extern void* stack_base;
+
+size_t get_stack_available(void);
+
+#define INIT_SAFE_ALLOCA() \
+do { \
+	int temp = 0; \
+	stack_base = &temp; \
+} while(0);
+
+#define SAFE_ALLOCA(size) ((size) < get_stack_available() ? alloca(size) : NULL)
+
+#ifdef SAFE_ALLOCA_IMPLEMENTATION
+
+void* stack_base = NULL;
+
+size_t get_stack_available(void) {
+		struct rlimit rlim;
+
+		if (getrlimit(RLIMIT_STACK, &rlim) != 0)
+				return 0;
+
+		if (rlim.rlim_cur == RLIM_INFINITY)
+				return SIZE_MAX;
+
+		int temp = 0;
+		void* stack_pointer = (void*)&temp;
+
+		// note: stack grows downwards
+		size_t stack_used = (size_t)stack_base - (size_t)stack_pointer;
+
+		if (stack_used >= rlim.rlim_cur)
+				return 0;
+
+		return rlim.rlim_cur - stack_used;
+}
+
+#endif
+#endif
---
+++ src/test.c
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define SAFE_ALLOCA_IMPLEMENTATION
+#include "safealloca.h"
+
+// some voodoo to tell the compiler not to optimise away redundant memory allocations
+// https://stackoverflow.com/questions/40122141/preventing-compiler-optimizations-while-benchmarking
+static void escape(void *p) {
+	__asm__ volatile("" : : "g"(p) : "memory");
+}
+
+__attribute__((noinline)) void run_unsafe_alloca(size_t size) {
+	char* buffer = (char*)alloca(size);
+	if (buffer == NULL)
+		return;
+
+	buffer[0] = 'A';
+	escape(buffer);
+}
+
+__attribute__((noinline)) void run_safe_alloca(size_t size) {
+	char* buffer = (char*)SAFE_ALLOCA(size);
+	if (buffer == NULL)
+		return;
+
+	buffer[0] = 'A';
+	escape(buffer);
+}
+
+__attribute__((noinline)) void run_malloc(size_t size) {
+	char* buffer = (char*)malloc(size);
+	if (buffer == NULL)
+		return;
+
+	buffer[0] = 'A';
+	escape(buffer);
+	free(buffer);
+}
+
+double get_time_diff(struct timespec start, struct timespec end) {
+	return (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+}
+
+void run_benchmark(size_t iterations, size_t allocation_size) {
+	struct timespec start, end;
+
+	printf("Benchmarking %zu iterations of allocating %zu bytes...\n", iterations, allocation_size);
+
+	// alloca
+	clock_gettime(CLOCK_MONOTONIC, &start);
+
+	for (size_t i = 0; i < iterations; i++) {
+		run_unsafe_alloca(allocation_size);
+	}
+
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	double unsafe_alloca_time = get_time_diff(start, end);
+
+	printf("alloca time: %f seconds\n", unsafe_alloca_time);
+
+	// SAFE_ALLOCA
+	clock_gettime(CLOCK_MONOTONIC, &start);
+
+	for (size_t i = 0; i < iterations; i++) {
+		run_safe_alloca(allocation_size);
+	}
+
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	double safe_alloca_time = get_time_diff(start, end);
+
+	printf("SAFEALLOCA time: %f seconds\n", safe_alloca_time);
+
+	// malloc
+	clock_gettime(CLOCK_MONOTONIC, &start);
+
+	for (size_t i = 0; i < iterations; i++) {
+		run_malloc(allocation_size);
+	}
+
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	double malloc_time = get_time_diff(start, end);
+
+	printf("malloc/free time: %f seconds\n", malloc_time);
+}
+
+int main(int argc, char* argv[]) {
+	INIT_SAFE_ALLOCA();
+
+	run_benchmark(10000000, 256);
+
+	return EXIT_SUCCESS;
+}
+
